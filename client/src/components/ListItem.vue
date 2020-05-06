b622ae43c614f27eb44bb16269200821f19fe463
Ashir <ashirq@outlook.com>
2020-05-06 18:48:51 +0100

    Added list item view

---
+++ client/src/components/ListItem.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="lesson">
+    <h3>{{ lesson.name }}</h3>
+    <img v-bind:src="lesson.image">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list-item',
+  props: ['lesson'],
+
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="css" scoped>
+
+.lesson {
+  display: flex;
+  flex-direction: column;
+}
+</style>
