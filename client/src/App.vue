a279543044714167470f95e29c63c1db9b039534
Alex Gyllos <alex.gyllos@gmail.com>
2020-05-06 18:36:23 +0100

    Adds App.vue, lessonservice

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <lesson-list></lesson-list>
+  </div>
+</template>
+
+<script>
+import LessonList from '@/components/LessonList.vue'
+import LessonService from '@/services/LessonService.js'
+import {eventBus} from '@/main.js';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      lessons: []
+    };
+  },
+  components: {
+    'lesson-list': LessonList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
