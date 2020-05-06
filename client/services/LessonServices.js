ce8beafb9fd08bb10be4b197bf1b7bb02bbedb3f
Jack Quigley <jackaquigley@gmail.com>
2020-05-06 19:03:41 +0100

    added postLesson function

---
+++ client/services/LessonServices.js
@@ -0,0 +1,17 @@
+const baseURL = 'http://localhost:3000/api/lessons/'
+
+export default {
+  getLessons(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+
+  postLesson(payload) {
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  }
+}
