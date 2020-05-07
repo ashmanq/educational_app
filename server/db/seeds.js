cf293a315846d16d66d3f6bfc066ea3787ae0535
Alex Gyllos <alex.gyllos@gmail.com>
2020-05-07 15:24:19 +0100

    updates solar system picture

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -64,7 +64,7 @@ db.lessons.insertMany(
         ]
     },
     { name: "Solar System",
-      image: "https://images.unsplash.com/photo-1506443432602-ac2fcd6f54e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+      image: "https://images.unsplash.com/photo-1485832329521-e944d75fa65e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
       details: [
     {
     name: "Mercury",
