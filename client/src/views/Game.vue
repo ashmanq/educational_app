a87d87c04f48f4f194dd7603fab41f00b0687f01
Alex Gyllos <alex.gyllos@gmail.com>
2020-05-08 17:33:08 +0100

    Updates UI for quiz

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,8 +1,11 @@
 <template lang="html">
-  <div class="flashcard">
-    <flashcards v-if="questions" v-for="(question, index) in questions" :question="question" :key="index" ></flashcards>
+  <div class="game">
+    <h1>Quiz</h1>
+    <div class="flashcard">
+      <flashcards v-if="questions" v-for="(question, index) in questions" :question="question" :key="index" ></flashcards>
 
-    <button v-on:click="checkAnswer" type="submit" name="button">Check Your Answers</button>
+      <button v-on:click="checkAnswer" type="submit" name="button">Check Your Answers</button>
+    </div>
   </div>
 
 </template>
@@ -45,8 +48,41 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style>
+  h1 {
+    color: white;
+  }
   .flashcard {
     color: white;
+    /* background-color: rgba(0, 0, 0, 0.5); */
+  }
+
+  .game {
+    width: 98vw;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+
+  button {
+    display: inline-block;
+    background-color: #f5ce42;
+    padding: 10px 20px;
+    font-family: sans-serif, Arial;
+    font-size: 16px;
+    border: 2px solid #444;
+    border-radius: 4px;
+    width: 250px;
+    /* text-shadow: -0.5px 0 black, 0 0.5px black, 0.5px 0 black, 0 -0.5px black; */
+    color: black;
+    margin-bottom: 50px;
+    transition: 0.1s;
+  }
+
+  button:hover {
+    background-color: white;
+    border-color: orange;
+    cursor: pointer;
   }
 </style>
