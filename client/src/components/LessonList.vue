fb1fbac76b119096958bf26a845a383f0d48847f
Alex Gyllos <alex.gyllos@gmail.com>
2020-05-10 12:05:54 +0100

    Adds progress bar functionality

--- client/src/components/LessonList.vue
+++ client/src/components/LessonList.vue
@@ -1,8 +1,15 @@
 <template lang="html">
    <div class="lesson-list">
      <h1>Lessons</h1>
-     <list-item v-for="(lesson, index) in lessons" :lesson="lesson" :key="index"></list-item>
-   </div>
+     <div class="meter">
+       <span :style="progress "></span>
+     </div>
+     <p>Completed: {{completedLessons}}/{{lessons.length}}</p>
+      <div class="lessons">
+        <list-item v-for="(lesson, index) in lessons" :lesson="lesson" :key="index"></list-item>
+      </div>
+  </div>
+
 </template>
 
 <script>
@@ -12,9 +19,25 @@ import ListItem from '@/components/ListItem.vue'
 export default {
   name: 'lesson-list',
   props: ['lessons'],
-
+  data() {
+    return {
+      completedLessons: 0,
+      progress: 0
+    }
+  },
   components: {
     'list-item': ListItem,
+  },
+  watch: {
+    lessons: function() {
+      this.lessons.forEach((lesson) => {
+        if (lesson.lesson_complete === 'true') {
+          this.completedLessons += 1
+        }
+      })
+      // setTimeout(2000)
+      this.progress = "width: " + ((this.completedLessons / this.lessons.length) * 100) + "%"
+    }
   }
 }
 </script>
@@ -22,14 +45,67 @@ export default {
 <style lang="css" scoped>
 
 
-.lesson-list {
-  display: flex;
-  justify-content: space-around;
-  justify-content: center;
-  /* margin: 5px; */
-  flex-wrap: wrap;
-  background-color: rgba(0, 0, 0, 0.6);
-  border-radius: 10px;
+  .lesson-list {
+    display: flex;
+    justify-content: space-around;
+    justify-content: center;
+    /* margin: 5px; */
+    flex-wrap: wrap;
+    background-color: rgba(0, 0, 0, 0.6);
+    border-radius: 10px;
+    flex-direction: column;
+  }
 
-}
+  p {
+    color: white;
+  }
+
+  .lessons {
+    display: flex;
+    flex-direction: row;
+    /* justify-content: space-around; */
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  .meter {
+  	height: 20px;  /* Can be anything */
+    width: 50vw;
+  	position: relative;
+  	background: #555;
+  	-moz-border-radius: 25px;
+  	-webkit-border-radius: 25px;
+  	border-radius: 25px;
+  	padding: 2px;
+  	box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3);
+    display: flex;
+    align-self: center;
+  }
+
+  .meter > span {
+  display: block;
+  height: 100%;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  background-color: rgb(43,194,83);
+  background-image: linear-gradient(
+    center bottom,
+    rgb(43,194,83) 37%,
+    rgb(84,240,84) 69%
+  );
+  box-shadow:
+    inset 0 2px 9px  rgba(255,255,255,0.3),
+    inset 0 -2px 6px rgba(0,0,0,0.4);
+  position: relative;
+  overflow: hidden;
+  animation: move 3s;
+  }
+
+  @keyframes move {
+   0% { width: 0; }
+   100% { width: 1; }
+ }
+
 </style>
